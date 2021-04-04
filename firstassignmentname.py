73f36ef56a123ed2477c6ffc665a604b13e121cf
PC Owner <niqevans@gmail.com>
2021-04-04 11:09:36 -0400

    Initial commit

---
+++ firstassignmentname.py
@@ -0,0 +1,9 @@
+
+# enter first name and last name
+names = input("Enter first and last name")
+print(names)
+print()
+# enter your goals for this class
+print("What are your goals for this class - long term?")
+goals = input("What would you like to be - when you complete this course?")
+print(goals)
